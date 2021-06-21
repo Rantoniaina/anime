67bedd5a3359ea534199e545e8991fe99759499f
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2021-06-21 21:50:26 +0300

    Develop services

--- frontend/anime/src/app/services/character/character.service.ts
+++ frontend/anime/src/app/services/character/character.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Character } from 'src/app/models/character';
+import { AppSettings } from 'src/app/settings/app.settings';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CharacterService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  findAllCharacters() {
+    return this.http.get(AppSettings.APP_URL + '/animes/');
+  }
+
+  findCharacterById(idCharacter: number) {
+    return this.http.get(AppSettings.APP_URL + '/animes/' + idCharacter);
+  }
+
+  saveCharacter(character: Character) {
+    return this.http.post(AppSettings.APP_URL + '/animes/', character);
+  }
+
+  shareCharacter(idCharacter: number, isShared: boolean) {
+    return this.http.get(
+      AppSettings.APP_URL + '/animes/share/' + idCharacter + '/' + isShared
+    );
+  }
 }
--- frontend/anime/src/app/services/user/user.service.ts
+++ frontend/anime/src/app/services/user/user.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { AppSettings } from 'src/app/settings/app.settings';
+import { User } from 'src/app/models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  findAllUsers() {
+    return this.http.get(AppSettings.APP_URL + '/users/');
+  }
+
+  findUserById(idUser: number) {
+    return this.http.get(AppSettings.APP_URL + '/users/' + idUser);
+  }
+
+  saveUser(user: User) {
+    this.http.post(AppSettings.APP_URL + '/users/', user);
+  }
+
+  login(mail: string, password: string) {
+    let params = new HttpParams();
+    params.append('mail', mail);
+    params.append('password', password);
+    return this.http.post(AppSettings.APP_URL + '/users/login', params);
+  }
 }
